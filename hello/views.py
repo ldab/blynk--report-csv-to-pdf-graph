67f94fbbaf179a63f2792711fea52e17c891e9a1
= <leonardo@corp.eurotec.co.nz>
2018-09-14 15:42:25 +1200

    remove buttons and added file upload

--- hello/views.py
+++ hello/views.py
@@ -1,14 +1,31 @@
 import requests
+from django import forms
 from django.shortcuts import render
 from django.http import HttpResponse
+
+#django form upload stuff
+from django.shortcuts import render
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
 from .models import Greeting
 
 # Create your views here.
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    #return HttpResponse('Hello from Python!')
+    #UploadFileform()
+    return render(request, 'index.html')
+
+def simple_upload(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'core/simple_upload.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    return render(request, 'core/simple_upload.html')
 
 def db(request):
 
