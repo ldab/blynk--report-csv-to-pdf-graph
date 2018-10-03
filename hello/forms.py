4b1cd58b4b6eef5adf32749e58db1eb73747347e
Leo <hello@untrol.io>
2018-10-03 23:55:04 +1300

    ,

--- hello/forms.py
+++ hello/forms.py
@@ -1,10 +1,10 @@
 from django import forms
-from models import Document
+#from models import Document
 
 class UploadFileForm(forms.Form):
     file  = forms.FileField()
 
-class DocumentForm(forms.ModelForm):
-    class Meta:
-        model = Document
-        fields = ('description', 'document', )
+#class DocumentForm(forms.ModelForm):
+#    class Meta:
+#        model = Document
+#        fields = ('description', 'document', )
