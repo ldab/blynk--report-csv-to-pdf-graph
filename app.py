d6e6acd8da12fba94082f9ff0d12a18b01735a3f
Leo <hello@untrol.io>
2018-10-08 15:06:43 +1300

    .

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ def allowed_file(filename):
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
+    from graph import tempFolder
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
